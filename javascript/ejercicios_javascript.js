3e9441b694032bfa79061a4e8fc4d3b6e9ab53f3
JoseBernal06 <133398097+MateoBernal06@users.noreply.github.com>
2025-03-11 13:39:42 -0500

    ejercicios bucles

--- javascript/ejercicios_javascript.js
+++ javascript/ejercicios_javascript.js
@@ -94,7 +94,7 @@ let nombres = ["Mateo", "Maria", "Daniela", "Carla"]
 }*/
 
 //* 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
-function conteoVocales(palabra){
+/*function conteoVocales(palabra){
     let contador_a= 0, contador_e=0, contador_i=0, contador_o=0, contador_u=0
     for (vocal of palabra) {
         if(vocal == "a"){
@@ -116,7 +116,7 @@ function conteoVocales(palabra){
     console.log(`a: ${contador_a}, e: ${contador_e}, i: ${contador_i}, o: ${contador_o}, u: ${contador_u}`)
 }
 
-conteoVocales("irlanda")
+conteoVocales("irlanda")*/
 
 
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
@@ -130,5 +130,24 @@ conteoVocales("irlanda")
 // 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
 
 
+//! ejercicios mouredev -- Funciones
 
+// 1. Crea una función que reciba dos números y devuelva su suma
 
+// 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
+
+// 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
+
+// 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
+
+// 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
+
+// 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
+
+// 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
+
+// 8. Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado
+
+// 9. Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
+
+// 10. Crea una función que calcule el factorial de un número dado
